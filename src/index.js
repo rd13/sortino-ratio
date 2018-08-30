51ac68ff5721b64ed319b8bc60e5a9c5c255bd4f
Stuart Rutter <stuart.rutter@metro.co.uk>
2018-08-30 11:55:57 +0100

    indentation

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@
  * @summary Sortino ratio
  * @description Calculate a Sortino ratio - a measure of risk similar to Sharpe ratio based on downside deviation
  * 
- * @param  {array} return_values asset/portfolio returns
- * @param  {number} risk free-risk rate (def: 0)
- * @param  {number} target_return minimum acceptable return (def: 0)
+ * @param  {array} return_values asset/portfolio return values
+ * @param  {number} risk free-risk rate
+ * @param  {number} target_return minimum acceptable return
  * @return {number}       
  *
  * @example
@@ -21,9 +21,9 @@ export default function sortino (return_values, target_return = 0) {
 	// 1)  Calculate the numerator of the 
 	// Sortino ratio, the average period 
 	// return minus the target return
-  const avg_return = return_values.reduce( ( a, b ) => a + b, 0 ) / return_values.length;
+	const avg_return = return_values.reduce( ( a, b ) => a + b, 0 ) / return_values.length;
 
-  const sortino_numerator = avg_return - target_return
+	const sortino_numerator = avg_return - target_return
 
 	// 2) For each data point, calculate
 	// the difference between that data
@@ -32,28 +32,28 @@ export default function sortino (return_values, target_return = 0) {
 	// the difference to 0%. The result of
 	// this step is the underperformance
 	// data set
-  const underperformance = return_values.map( p => Math.min( target_return, p - target_return ) )
+	const underperformance = return_values.map( p => Math.min( target_return, p - target_return ) )
 
 	// 3) Calculate the square of each
 	// value in the underperformance data
 	// set 
-  const underperformance_squared = underperformance.map( p => Math.pow(p, 2) )
+	const underperformance_squared = underperformance.map( p => Math.pow(p, 2) )
 
 	// 4) Calculate the average of all
 	// squared differences determined in
 	// Step 3
-  const underperformance_squared_average = underperformance_squared.reduce( ( a, b ) => a + b, 0 ) / underperformance_squared.length
+	const underperformance_squared_average = underperformance_squared.reduce( ( a, b ) => a + b, 0 ) / underperformance_squared.length
 
 	// 5) Take the square root of the
 	// average determined in Step 4. This
 	// is the target downside deviation
 	// used in the denominator of the
 	// Sortino ratio
-  const target_downside_deviation = Math.sqrt(underperformance_squared_average)
+	const target_downside_deviation = Math.sqrt(underperformance_squared_average)
 
 	// 6) Calculate the Sortino ratio
-  const sortino_ratio = avg_return / target_downside_deviation
-  
-  return sortino_ratio
+	const sortino_ratio = avg_return / target_downside_deviation
+
+	return sortino_ratio
 }
 
